2db90f85f99a4e228b87d765608eaa7a0039d1dd
Nelson Mecha <nelsonorinamecha@gmail.com>
2024-04-26 21:21:53 +0300

    filtering purchases

--- src/App.js
+++ src/App.js
@@ -6,42 +6,17 @@ import FilterPurchases from './filter';
 
 function App() {
 
-  const data =[
-    {
-     Date: '2/11/2024',
-     Description: 'Movie',
-     Category:'Entertainement',
-     Amount: '$40'
-    },
-    {
-    Date: '4/11/2024',
-    Description: 'Chipotle',
-    Category:'Food',
-    Amount: '$70'
-    },
-    {
-     Date:'5/11/2024',
-     Description:'Delta Airlines',
-     Category:'Trip',
-     Amount:'$200'  
-    },
-    {
-     Date:'5/11/2024',
-     Description:'Airpods',
-     Category:'Electronics',
-     Amount:'$100'   
-    }
-]
+  
 
   return(
     <>
      <div className='container-fluid bg-primary text-center'>
       <h1 className='bank_name'>The Royal Bank of Flatiron </h1>
      </div>
-     <FilterPurchases info={data}/>
-     <Table info={data}/>
+     <FilterPurchases />
     </>
   );
 }
+
 
 export default App;
--- src/filter.js
+++ src/filter.js
@@ -1,13 +1,55 @@
-function FilterPurchases ({info}) {
-    //console.log(info);
-    function filterByCategory () {
-        //console.log('working')
-        const newArray = info.filter((purchase) => {
-            
+import { useState } from "react";
+import Table from "./Table";
+function FilterPurchases() {
+    const array =[
+        {
+            Date: '2/11/2024',
+            Description: 'Movie',
+            Category:'Entertainement',
+            Amount: '$40'
+           },
+           {
+           Date: '4/11/2024',
+           Description: 'Chipotle',
+           Category:'Food',
+           Amount: '$70'
+           },
+           {
+            Date:'5/11/2024',
+            Description:'Delta Airlines',
+            Category:'Trip',
+            Amount:'$200'  
+           },
+           {
+            Date:'5/11/2024',
+            Description:'Airpods',
+            Category:'Electronics',
+            Amount:'$100'   
+           }
+    ]
+
+
+    const [data, setData] = useState(array)
+
+
+    function filterByDescription (event){
+        const text = event.target.value
+       if(text.length > 0){
+        const filtered = data.filter((purchase) => {
+            return purchase.Description.toLowerCase().includes(text.toLowerCase())
         })
+        setData([...filtered])
+       }else{
+        setData([...array])
+       }
     }
+     
     return(
-        <input onChange={filterByCategory} className="form-control m-4 p-2" type="text" placeholder="filter by Category"/>
+        <>
+         <input onChange={filterByDescription} className="form-control m-4 p-2" type="text" placeholder="filter by Description"/>
+         <Table info={data}/>
+        </>
+        
     )
 }
 
